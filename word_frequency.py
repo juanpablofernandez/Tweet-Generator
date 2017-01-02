5c9fc74e024153800d8b36fcc83885cfbbe76ee5
Jay <jaypee.1797@gmail.com>
2017-01-01 23:35:37 -0600

    Finished Challenge 3

---
+++ word_frequency.py
@@ -0,0 +1,27 @@
+import re
+
+def histogram(text):
+    txt = open(text)
+    book = txt.read()
+    histogram = {}
+    for word in book.split():
+        word = word.lower()
+        words = re.findall(r"[\w']+", word)
+        if len(words) != 0:
+            word = words[0]
+            if word not in histogram:
+                 histogram[word] = 1
+            else:
+                histogram[word] = histogram[word] + 1
+    print histogram
+    return histogram
+
+def unique_words(histogram):
+    print len(histogram)
+
+def frequency(word, histogram):
+    print histogram[word]
+
+histograms = histogram("holmes.txt")
+unique_words(histograms)
+frequency("could", histograms)
