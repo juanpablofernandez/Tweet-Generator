48c98bf5b6b54284166cbf4c7f4331ae8fc01f11
Jay <jaypee.1797@gmail.com>
2017-01-03 20:04:06 -0600

    Fixed app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 import word_probability
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+
+def run():
     histogram = word_probability.word_list
     sentence = word_probability.get_random_sentence(histogram, 17)
     return sentence
+
+if __name__ == "__main__":
+    run()
