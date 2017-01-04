29980525fd08c0a74206302dab1eea0aba4a3e48
Jay <jaypee.1797@gmail.com>
2017-01-03 20:49:27 -0600

    More issues..

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 import word_probability
 
 app = Flask(__name__)
@@ -12,5 +11,4 @@ def start_app():
     return sentence
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run()
