42562765f96c98b532ab31b573f703aa21fa0c4a
Jay <jaypee.1797@gmail.com>
2017-01-03 20:22:05 -0600

    Fixed some flask issues

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import word_probability
 app = Flask(__name__)
 
 @app.route('/')
-
-def run():
+def start_app():
+    print "Running..."
     histogram = word_probability.word_list
     sentence = word_probability.get_random_sentence(histogram, 17)
     return sentence
 
 if __name__ == "__main__":
-    run()
+    app.run()
